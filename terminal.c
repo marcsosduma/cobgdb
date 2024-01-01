d99efcc7da62ede9761f32000bac9d15ae234ce5
marcsosduma <31162117+marcsosduma@users.noreply.github.com>
2024-01-01 19:49:49 -0300

    Small corrections

--- terminal.c
+++ terminal.c
@@ -637,36 +637,6 @@ void SetConsoleSize(HANDLE hStdout, int cols, int rows )
         return;
   }
 }
-
-void cls_screen(HANDLE hConsole)
-{
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    SMALL_RECT scrollRect;
-    COORD scrollTarget;
-    CHAR_INFO fill;
-
-    // Get the number of character cells in the current buffer.
-    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)){
-        return;
-    }
-    // Scroll the rectangle of the entire buffer.
-    scrollRect.Left = 0;
-    scrollRect.Top = 0;
-    scrollRect.Right = csbi.dwSize.X;
-    scrollRect.Bottom = csbi.dwSize.Y;
-    // Scroll it upwards off the top of the buffer with a magnitude of the entire height.
-    scrollTarget.X = 0;
-    scrollTarget.Y = (SHORT)(0 - csbi.dwSize.Y);
-    // Fill with empty spaces with the buffer's default text attribute.
-    fill.Char.UnicodeChar = TEXT(' ');
-    fill.Attributes = csbi.wAttributes;
-    // Do the scroll
-    ScrollConsoleScreenBuffer(hConsole, &scrollRect, NULL, scrollTarget, &fill);
-    // Move the cursor to the top left corner too.
-    csbi.dwCursorPosition.X = 0;
-    csbi.dwCursorPosition.Y = 0;
-    SetConsoleCursorPosition(hConsole, csbi.dwCursorPosition);
-}
 #endif
 
 char * get_textcolor_code(const int textcolor) { // Linux only
